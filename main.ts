d34a4bffb06f72e4041a1cd1e51d19040518717c
ilosz221 <102656790+ilosz221@users.noreply.github.com>
2022-03-30 11:05:45 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.B, function () {
+    teller = 0
+})
+input.onPinPressed(TouchPin.P1, function () {
+    teller += 1
+    basic.showIcon(IconNames.Sad)
+    music.playTone(880, music.beat(BeatFraction.Whole))
+})
+let teller = 0
+teller = 0
 basic.forever(function () {
-	
+    basic.showNumber(teller)
 })
